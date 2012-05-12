b2d38f1118ff993045bc30289856c509b5833042
Kevin Lin <kevin.lin.p@gmail.com>
2012-05-11 20:26:36 -0700

    DotJS
    Refactored Steam script for easier reading.

--- dotjs/store.steampowered.com.js
+++ dotjs/store.steampowered.com.js
@@ -1,14 +1,14 @@
 if ($('.transactionRowPrice').length != 0)
 {
-	total = 0.0;
 
 	totaler = function(e,i){
 		var regex = /(\d+\.\d\d+)/;
 		price = regex.exec($(e).html());
-		if (price != null) {
-			total += parseFloat(price[0])}
+		if (price != null) { return parseFloat(price);}
 	};
-	jQuery.map($('.transactionRowPrice'), totaler);
+	prices = jQuery.map($('.transactionRowPrice'), totaler);
 
-	alert("You are a $" + total + " customer.");
+	total = 0.0;
+	jQuery.map(prices, function(e,i){total += e});
+	$('.transactionRowPrice').first().text("Total: $" + total);
 }
